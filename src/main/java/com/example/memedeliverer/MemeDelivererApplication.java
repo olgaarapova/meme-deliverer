4a029735a33da874c8528338e1d7d8be4bd41128
olga <arapovaolga1999@gmail.ru>
2021-08-14 17:11:14 +0500

    init

---
+++ src/main/java/com/example/memedeliverer/MemeDelivererApplication.java
@@ -0,0 +1,13 @@
+package com.example.memedeliverer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeDelivererApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemeDelivererApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/memedeliverer/MemeDelivererApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.memedeliverer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemeDelivererApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
